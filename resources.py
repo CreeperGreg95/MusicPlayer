bd4e23785ee5321df63107cd3c1e72d944ce8e5b
Grégory <gregory95am@gmail.com>
2024-11-24 22:53:30 +0100

    0.0.0.1

--- resources.py
+++ resources.py
@@ -1,12 +1,12 @@
 # Ce fichier contient les chemins des ressources utilisées dans le projet
 
 # Icônes des boutons
-HOME_ICON = "assets/icons/home_icon.png"
-PROFILE_ICON = "assets/icons/profile_icon.png"
-NOTIFICATION_ICON = "assets/icons/notification_icon.png"
-MENU_ICON = "assets/icons/menu_icon.png"
-BACK_ICON = "assets/icons/back_icon.png"
-FORWARD_ICON = "assets/icons/forward_icon.png"
+HOME_ICON = "assets/icons/home_icon.png" # Coming soon
+PROFILE_ICON = "assets/icons/profile_icon.png" # Coming soon
+NOTIFICATION_ICON = "assets/icons/notification_icon.png" # Coming soon
+MENU_ICON = "assets/icons/menu_icon.png" # Coming soon
+BACK_ICON = "assets/icons/back_icon.png" # Coming soon
+FORWARD_ICON = "assets/icons/forward_icon.png" # Coming soon
 
 # Icône de la fenêtre
 WINDOW_ICON = "assets/icons/spotifylogo.jpg"
